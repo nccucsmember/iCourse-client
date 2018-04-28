0179ca7b4ea8e0504f098d1a2866e0b5c783ecfa
WilsonWang <wilson8507@gmail.com>
2018-04-28 17:23:54 +0800

    Add sematic-ui link

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default (
-    <h1>Hello World!</h1>
-);
+export default [
+  <h1>Hello world</h1>,
+  <div className="ui three buttons">
+    <button className="ui button">One</button>
+    <button className="ui button">Two</button>
+    <button className="ui button">Three</button>
+  </div>,
+];
 
