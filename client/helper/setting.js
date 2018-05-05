8cd394bf7c4eaaebed78ccae343c0ed0cabf466e
WilsonWang <wilson8507@gmail.com>
2018-05-05 13:04:31 +0800

    Add SITE_LINKS, images, and react-router-dom

---
+++ client/helper/setting.js
@@ -0,0 +1,14 @@
+export const SITE_LINKS = [{
+  id: 'course',
+  name: '課程清單',
+  subLinks: [],
+}, {
+  id: 'select',
+  name: '選課清單',
+  subLinks: [],
+}, {
+  id: 'trace',
+  name: '追蹤清單',
+  subLinks: [],
+}];
+export default SITE_LINKS;
