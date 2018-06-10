fdf5e872510dba71829d1160c5654f2271fff62d
Chih-Hsuan, Kuo <appale1234@gmail.com>
2018-06-10 15:11:44 +0800

    add text error message

--- client/actions/Register.js
+++ client/actions/Register.js
@@ -2,7 +2,7 @@ import { API_REQUEST } from 'redux-middleware-fetch';
 
 export const REGISTER = Symbol('REGISTER');
 
-export function register(payload) {
+export function register(payload, onFailed) {
   return {
     [API_REQUEST]: {
       types: [
@@ -12,6 +12,9 @@ export function register(payload) {
       json: true,
       method: 'POST',
       entrypoint: '/api/v1/register',
+      onFailed: () => {
+        onFailed('請確認密碼是否大於六個字元');
+      },
     },
   };
 }
